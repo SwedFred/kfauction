a9cf03ae6457c4face37887be099a26c046dbeb1
Fredrik Arvidsson <fredrik.areblad@gmail.com>
2022-04-13 09:59:06 +0200

    Initial commit

---
+++ refine-data_kronofogden.ps1
@@ -0,0 +1,29 @@
+# Inputs the Kronofogden project CSV file and removes whitespaces from the number fields
+# this is to avoid truncation when importing the values to the database
+$filename = Read-Host "Enter CSV file-name for the Kronofogden data project"
+$ext = [IO.Path]::GetExtension($filename)
+if ($ext -ne ".csv") {$filename = $filename + ".csv"} # Add file ending if not present
+if (Test-Path -Path ((Get-Location).ToString() + "/" + $filename)){
+  try {
+    $csvfile = Import-Csv -Path ./$filename -UseCulture -Encoding Default
+  }
+  catch {
+    throw "Importing $filename failed"
+  }
+  foreach ($row in $csvfile) {
+    $row."Utropspris (kr)" = $row."Utropspris (kr)" -replace '\s', ''
+    $row."Startpris (kr)" = $row."Startpris (kr)" -replace '\s', ''
+    $row."Betalat belopp (kr)" = $row."Betalat belopp (kr)" -replace '\s', ''
+  }
+  try {
+    $csvfile | Export-Csv -Path ./clean.csv -NoTypeInformation -Encoding Default -UseCulture
+  }
+  catch {
+    throw "Exporting to file Clean.csv failed. Make sure it's not opened somewhere!"
+  }
+  Write-Host "Success!"
+}
+else {
+  Write-Host "Could not verify file name $filename"
+}
+
